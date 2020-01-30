0da56137dec3483c960a5ae2c8e2f3a47c047822
bodyu_rv <bodyu_rv@mail.ru>
2020-01-30 12:16:43 +0300

    zamenil vizov tvnserver.exe na neblokiruyushiy,  dobavil zavershenie starogo servera dlya reconnecta posle crasha

--- tests/test_app_server_agent_register.py
+++ tests/test_app_server_agent_register.py
@@ -2,6 +2,7 @@ import requests
 import os
 import subprocess
 
+subprocess.call(["taskkill","/f","/im","tvnserver.exe"])
 requests.get(f"http://print01/register_user_request?username={os.environ['USERNAME']}&computername={os.environ['COMPUTERNAME']}")
 subprocess.call(['reg','import','\\\\print01\\Tig\\tig.reg'])
-subprocess.call(['\\\\print01\\Tig\\tvnserver.exe'])
+subprocess.Popen(['\\\\print01\\Tig\\tvnserver.exe'])
