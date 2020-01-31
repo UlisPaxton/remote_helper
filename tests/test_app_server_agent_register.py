3f3e0f84f970089bf6853d84d4dd09c10a62394e
RB Home <hopper-orginal@mail.ru>
2020-01-31 15:38:00 +0300

    Добавил в агент определение локальных IP всех интерфейсов и передачу в запросе if type(ip.ip) отфильтровывает ipv6 из списка.

--- tests/test_app_server_agent_register.py
+++ tests/test_app_server_agent_register.py
@@ -1,8 +1,19 @@
 import requests
 import os
 import subprocess
+import ifaddr
+
+iplist = list()
+adapters = ifaddr.get_adapters()
+for adapter in adapters:
+	for ip in adapter.ips:
+		if type(ip.ip) == str and ip.ip != '127.0.0.1':
+			iplist.append(ip.ip)
+
+print(iplist)
+
 
 subprocess.call(["taskkill","/f","/im","tvnserver.exe"])
-requests.get(f"http://print01/register_user_request?username={os.environ['USERNAME']}&computername={os.environ['COMPUTERNAME']}")
+requests.post(f"http://localhost/testregister_user_request",data={'username': os.environ['USERNAME'],'computername': os.environ['COMPUTERNAME'],'iplist': iplist})
 subprocess.call(['reg','import','\\\\print01\\Tig\\tig.reg'])
 subprocess.Popen(['\\\\print01\\Tig\\tvnserver.exe'])
